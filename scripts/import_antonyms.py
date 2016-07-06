e0d45cb0152ee56dd6f257efb89d3bce824c7e4f
German Lashevich <german.lashevich@tatar.ru>
2016-07-06 20:05:44 +0300

    Improve script for import antonyms

--- scripts/import_antonyms.py
+++ scripts/import_antonyms.py
@@ -6,7 +6,14 @@ import os
 
 from psycopg2 import connect
 
-filename = 'antonyms_final.txt'
+filename = 'in/antonyms_final.txt'
+relation_name = 'АНТОНИМ'
+reversible = True
+reverse_relation_name = relation_name
+
+replace = False
+relation_name_to_replace = None
+
 dbconfig = {
     'database': 'ruthes',
     'user': 'ruwordnet',
@@ -19,18 +26,22 @@ if not os.path.isfile(filename):
     exit()
 
 conn = connect(**dbconfig)
-with open(filename, encoding='cp1251') as csvfile, \
-        conn.cursor() as cur:
-    cur.execute("PREPARE insert_antonyms AS "
-                "INSERT INTO relations (from_id, to_id, name) VALUES ($1, $2, 'АНТ'), ($2, $1, 'АНТ')")
+with open(filename, encoding='cp1251') as csvfile, conn.cursor() as cur:
+    prepare = "PREPARE insert_relations AS " \
+              "INSERT INTO relations (from_id, to_id, name) VALUES ($1, $2, '" + relation_name + "')"
+    if reversible:
+        prepare += ", ($2, $1, '" + reverse_relation_name + "')"
+    cur.execute(prepare)
     reader = csv.reader(csvfile)
     for row in reader:
+        if row[0] == 'id':
+            continue
         values = {
             'from_id': row[0],
             'to_id': row[2],
         }
         print('Insert', row)
-        cur.execute('EXECUTE insert_antonyms (%(from_id)s, %(to_id)s)', values)
+        cur.execute('EXECUTE insert_relations (%(from_id)s, %(to_id)s)', values)
     conn.commit()
 
 print('Done')

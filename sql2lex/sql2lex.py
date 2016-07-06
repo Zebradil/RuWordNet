b906143cee55c75e073e8b3f157874f73796ed7e
German Lashevich <german.lashevich@tatar.ru>
2016-07-06 20:09:30 +0300

    Improve compatibility with Wordnet lex-files format in sql2lex utility

--- sql2lex/sql2lex.py
+++ sql2lex/sql2lex.py
@@ -54,127 +54,28 @@ def main(argv):
     print('Done')
 
 
-def generate_lex_file_empty(pos):
-    types = {
-        'N': ('N', 'NG')
-    }[pos]
-    lex_file = {
-        'N': 'noun.all'
-    }[pos]
-    empty_word = 'empty{0}"'
-    filename = os.path.join(OUT_ROOT, lex_file)
-    if os.path.isfile(filename):
-        os.remove(filename)
-    print('Output file: ' + filename)
-    with conn.cursor(cursor_factory=extras.RealDictCursor) as cur, \
-            open(filename, 'w', encoding='utf-8') as file:
-
-        print('Finding entries...')
-        sql = """
-          SELECT
-            c.id   c_id,
-            c.name c_name,
-            c.gloss,
-            t.id,
-            t.name,
-            t.synt_type,
-            array_remove(
-                array_agg(DISTINCT s2.concept_id),
-                NULL
-            )      concept_ids,
-            array_remove(
-                array_agg(DISTINCT s3.entry_id),
-                NULL
-            )      entry_ids
-          FROM synonyms s
-            LEFT JOIN text_entry t
-              ON t.id = s.entry_id
-              AND t.synt_type IN %(types)s
-            LEFT JOIN synonyms s2
-              ON s2.entry_id = t.id
-            INNER JOIN synonyms s3
-              ON s3.concept_id = s.concept_id
-            INNER JOIN concepts c
-              ON c.id = s.concept_id
-          GROUP BY t.id, c.id
-          ORDER BY t.name NULLS LAST"""
-        cur.execute(sql, {'types': types})
-
-        rgxSpace = re.compile('([,()"]|\s+)')
-        rgxEscape = re.compile('([,()])')
-        empty_cnt = 1
-        concepts = {}
-        for row in cur:
-            cid = row['c_id']
-            if row['id'] is None:
-                empty_cnt += 1
-                row['name'] = empty_word.format(empty_cnt)
-            else:
-                row['name'] = rgxSpace.sub('_', row['name'].strip())
-                # row['name'] = rgxEscape.sub(r'\\\1', row['name'])
-                if len(row['concept_ids']) > 1:
-                    row['name'] += str(row['concept_ids'].index(cid) + 1)
-            if cid not in concepts:
-                concept = {
-                    'id': cid,
-                    'name': row['c_name'],
-                    'gloss': row['gloss'],
-                    'relations': [],
-                    'entries': []
-                }
-                concepts[cid] = concept
-            concepts[cid]['entries'].append({k: row[k] for k in ('id', 'name', 'synt_type')})
-        print('{0} entries found. {1} are empty.'.format(cur.rowcount, empty_cnt))
-
-        print('Selecting relations...')
-        sql = 'SELECT * FROM relations'
-        cur.execute(sql)
-
-        for relation in cur:
-            cid = relation['from_id']
-            # бывает, связь есть, а понятия такого нет
-            if cid in concepts:
-                concepts[cid]['relations'].append(relation)
-
-        synset_tpl = '{{{words}{pointers} ({gloss})}}'
-
-        count = len(concepts)
-        i = 0
-        print('Processing concepts ({0}) and relations...'.format(count))
-        for cid, concept in concepts.items():
-            i += 1
-            gloss = concept['name'] + (' | ' + xstr(concept['gloss']) if concept['gloss'] is not None else '')
-            gloss = rgxSpace.sub('_', gloss)
-            words = [entry['name'] + ',' for entry in concept['entries'] if entry['id'] is not None]
-            if not words:
-                words = concept['entries'][0]['name'] + ','
-
-            pointers = []
-            for relation in concept['relations']:
-                ptr_chr = get_pointer(relation['name'], relation['asp'], 'N')
-                tid = relation['to_id']
-                if ptr_chr is not None and tid in concepts:
-                    ptr_word = get_pointer_word(concepts[tid]['entries'])
-                    pointers.append(ptr_word + ',' + ptr_chr)
-
-            synset = synset_tpl.format(words=''.join(words), pointers=' '.join(pointers), gloss=gloss)
-            file.write(synset + '\n')
-            print('\rProgress: {0}% ({1})'.format(round(i / count * 100), i), end='', flush=True)
-        print()
-
-
 def generate_lex_file(pos):
-    types = {
+    all_types = {
         'N': ('N', 'NG', 'NGprep', 'PrepG'),
         'V': ('V', 'VG', 'VGprep', 'Prdc'),
         'Adj': ('Adj', 'AdjG', 'AdjGprep'),
-
-    }[pos]
-    lex_file = {
+    }
+    types = all_types[pos]
+    lex_files = {
         'N': 'noun.all',
         'V': 'verb.all',
         'Adj': 'adj.all',
-    }[pos]
+    }
+    lex_file = lex_files[pos]
+
+    if pos == 'N':
+        derivational_types = all_types['V']
+        d_file = lex_files['V']
+    elif pos == 'V':
+        derivational_types = all_types['N']
+        d_file = lex_files['N']
+    else:
+        derivational_types = None
 
     filename = os.path.join(OUT_ROOT, lex_file)
 
@@ -205,22 +106,20 @@ def generate_lex_file(pos):
                 NULL
             )      entry_ids
           FROM synonyms s
-            LEFT JOIN text_entry t
+            INNER JOIN text_entry t
               ON t.id = s.entry_id
-              AND t.synt_type IN %(types)s
-            LEFT JOIN synonyms s2
+            INNER JOIN synonyms s2
               ON s2.entry_id = t.id
             INNER JOIN synonyms s3
               ON s3.concept_id = s.concept_id
-            INNER JOIN concepts c
-              ON c.id = s.concept_id
-          GROUP BY t.id, c.id
+            INNER JOIN concepts C
+              ON C.id = s.concept_id
+          GROUP BY t.id, C.id
           ORDER BY t.name NULLS LAST"""
-        cur.execute(sql, {'types': types})
+        cur.execute(sql)
 
         rgxSpace = re.compile('([,()"\s]+)')
-        rgxEscape = re.compile('([,()])')
-        empty_cnt = 1
+        empty_cnt = 0
         concepts = {}
         for row in cur:
             cid = row['c_id']
@@ -237,10 +136,14 @@ def generate_lex_file(pos):
                     'name': row['c_name'],
                     'gloss': row['gloss'],
                     'relations': [],
-                    'entries': []
+                    'entries': [],
+                    'all_entries': [],
                 }
                 concepts[cid] = concept
-            concepts[cid]['entries'].append({k: row[k] for k in ('id', 'name', 'synt_type')})
+            concepts[cid]['all_entries'].append({k: row[k] for k in ('id', 'name', 'synt_type')})
+            # Фильтруем текстовые входы, оставляя только определённую часть речи
+            if row['synt_type'] in types:
+                concepts[cid]['entries'].append({k: row[k] for k in ('id', 'name', 'synt_type')})
 
         print('{0} entries found. {1} are empty.'.format(cur.rowcount, empty_cnt))
 
@@ -252,14 +155,16 @@ def generate_lex_file(pos):
               ON cf.id = r.from_id
             INNER JOIN concepts ct
               ON ct.id = r.to_id
-          WHERE r.name IN('ВЫШЕ', 'НИЖЕ')
         """
         cur.execute(sql)
 
         for relation in cur:
             concepts[relation['from_id']]['relations'].append(relation)
 
-        synset_tpl = '{{{words},{pointers} ({gloss})}}'
+        if pos == 'V':
+            synset_tpl = '{{{words},{pointers} frames: 1 ({gloss})}}'
+        else:
+            synset_tpl = '{{{words},{pointers} ({gloss})}}'
 
         count = len(concepts)
         i = 0
@@ -280,6 +185,8 @@ def generate_lex_file(pos):
             for relation in concept['relations']:
                 relations += fix_relation(concepts, relation)
 
+            relations = uniqify(relations, lambda r: "{to_id}|{name}".format(**r))
+
             pointers = []
             for relation in relations:
                 ptr_chr = get_pointer(relation['name'], relation['asp'], pos)
@@ -288,11 +195,50 @@ def generate_lex_file(pos):
                     ptr_word = get_pointer_word(toc['entries'])
                     pointers.append(ptr_word + ',' + ptr_chr)
 
+            # Отдельно добавляем указатели для словообразовательных отношений
+            if derivational_types:
+                pointers += gen_derivational_pointers(derivational_types, concept, d_file)
+            else:
+                for entry in concept['all_entries']:
+                    est = entry['synt_type']
+                    if est in all_types['N']:
+                        d_file = lex_files['N']
+                        ptr_chr = '\\'
+                    elif est in all_types['V']:
+                        d_file = lex_files['V']
+                        ptr_chr = '<'
+                    else:
+                        continue
+                    pointers += ['{0}:{1},{2}'.format(d_file, entry['name'], ptr_chr)]
+
             words = [entry['name'] for entry in concept['entries']]
             synset = synset_tpl.format(words=','.join(words), pointers=' '.join(pointers), gloss=gloss)
             file.write(synset + '\n')
             print('\rProgress: {0}% ({1})'.format(round(i / count * 100), i), end='', flush=True)
         print()
+
+
+def gen_derivational_pointers(derivational_types, concept, d_file):
+    for d_type in derivational_types:
+        for entry in concept['all_entries']:
+            if entry['synt_type'] == d_type:
+                return ['{0}:{1},+'.format(d_file, entry['name'])]
+    return []
+
+
+def uniqify(seq, idfun=None):
+    # order preserving
+    if idfun is None:
+        def idfun(x): return x
+    seen = {}
+    result = []
+    for item in seq:
+        marker = idfun(item)
+        if marker in seen:
+            continue
+        seen[marker] = 1
+        result.append(item)
+    return result
 
 
 def fix_relation(concepts, relation, path=None) -> object:
@@ -302,6 +248,7 @@ def fix_relation(concepts, relation, path=None) -> object:
     проверку, спускаемся по иерархии отношений вниз и повторяем
     проверку для низлежащих отношений.
 
+    :param path:
     :param concepts:
     :param relation:
     :return:
@@ -318,7 +265,11 @@ def fix_relation(concepts, relation, path=None) -> object:
         if len([entry for entry in toc['entries'] if entry['id'] is not None]) > 0:
             # Если у понятия есть не пустые текстовые входы, значит отношение нам подходит
             return [relation]
-        # Отношение не подходит - спускаемся ниже по иерархии
+        # Отношение не подходит
+        # Замыкание предусмотрено не для всех типов связей
+        if relation['name'] not in ['НИЖЕ', 'ВЫШЕ']:  # , 'ЭКЗЕМПЛЯР', 'КЛАСС', 'ЦЕЛОЕ', 'ЧАСТЬ']:
+            return []
+        # Спускаемся ниже по иерархии
         relations = []
         # Смотрим все отношения низлежащего понятия
         for rel in toc['relations']:
@@ -332,16 +283,48 @@ def fix_relation(concepts, relation, path=None) -> object:
 
 def get_pointer(rel_type, asp, pos):
     rel_map = {
-        'АСЦ2': None,
-        'ЦЕЛОЕ': '#p',
-        'АСЦ1': None,
-        'ЧАСТЬ': '%p',
-        'НИЖЕ': '~',
-        'ВЫШЕ': '@',
-        'АСЦ': None,
-        'АНТ': '!',
+        'N': {
+            'АСЦ2': None,
+            'ЦЕЛОЕ': '#p',
+            'АСЦ1': None,
+            'ЧАСТЬ': '%p',
+            'НИЖЕ': '~',
+            'ВЫШЕ': '@',
+            'АСЦ': None,
+            'АНТОНИМ': '!',
+            'ЭКЗЕМПЛЯР': '~i',
+            'КЛАСС': '@i',
+        },
+        'V': {
+            'АСЦ2': None,
+            'ЦЕЛОЕ': '*',
+            'АСЦ1': None,
+            'ЧАСТЬ': '*',
+            'НИЖЕ': '~',
+            'ВЫШЕ': '@',
+            'АСЦ': None,
+            'АНТОНИМ': '!',
+            'ЭКЗЕМПЛЯР': None,
+            'КЛАСС': None,
+        },
+        'Adj': {
+            'АСЦ2': None,
+            'ЦЕЛОЕ': None,
+            'АСЦ1': None,
+            'ЧАСТЬ': None,
+            'НИЖЕ': '&',
+            'ВЫШЕ': '&',
+            'АСЦ': None,
+            'АНТОНИМ': '!',
+            'ЭКЗЕМПЛЯР': None,
+            'КЛАСС': None,
+        }
     }
-    return rel_map[rel_type]
+
+    if rel_type in ('ЧАСТЬ', 'ЦЕЛОЕ') and asp == '':
+        return None
+
+    return rel_map[pos][rel_type]
 
 
 def get_pointer_word(entries):

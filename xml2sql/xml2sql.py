3b80d9b97c0691e20a2c8292f707fe9cde6296fd
German Lashevich <german.lashevich@tatar.ru>
2017-02-01 15:12:31 +0300

    Rewrite xml2sql.py

--- xml2sql/xml2sql.py
+++ xml2sql/xml2sql.py
@@ -1,76 +1,43 @@
-#!/usr/bin/python3
-# coding=utf-8
-
-import getopt
+#!/usr/bin/env python3
+import argparse
 import os
-import sys
-from subprocess import Popen, PIPE
 from xml.etree import ElementTree
 
-from psycopg2 import connect
+import psycopg2
 
 PKG_ROOT = os.path.split(__file__)[0]
-SQL_ROOT = os.path.join(PKG_ROOT, 'sql')
-# XML_ROOT = os.path.join(PKG_ROOT, 'xml')
-XML_ROOT = os.path.join(PKG_ROOT, '..', 'raw2xml', 'out')
 
-conn = None
+parser = argparse.ArgumentParser(description='Run import RuThes from xml to database.')
+parser.add_argument(
+    '-s',
+    '--xml-dir',
+    type=str,
+    help='Source xml root directory',
+    default=os.path.join(PKG_ROOT, 'xml')
+)
+parser.add_argument(
+    '-l',
+    '--log-dir',
+    type=str,
+    help='Log files destination',
+    default=os.path.join(PKG_ROOT, 'log')
+)
+connection_string = "host='localhost' dbname='ruwordnet' user='ruwordnet' password='ruwordnet'"
+parser.add_argument(
+    '-c',
+    '--connection-string',
+    type=str,
+    help="Postgresql database connection string ({})".format(connection_string),
+    default=connection_string
+)
+# parser.add_argument(
+#     '-v',
+#     dest='verbose',
+#     help='verbose output',
+#     action='store_true'
+# )
 
-dbconfig = {
-    'database': 'ruthes',
-    'user': 'ruwordnet',
-    'password': 'ruwordnet',
-    'host': '127.0.0.1'
-}
-
-
-def main(argv):
-    global XML_ROOT, conn
-
-    help_str = 'Usage: {0} [-h] [--xml-root=<xml_root_directory>]'.format(os.path.split(__file__)[1])
-    try:
-        opts, args = getopt.getopt(argv, "h", ["xml-root="])
-    except getopt.GetoptError:
-        print(help_str)
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print(help_str)
-            sys.exit()
-        elif opt == '--xml-root':
-            XML_ROOT = arg
-
-    try:
-        conn = connect(**dbconfig)
-    except:
-        print('I am unable to connect to the database')
-        exit(1)
-
-    init_db()
-    import_data()
-    create_indexes()
-
-    print('Done')
-
-
-def run_sql_file(filename):
-    print('Run sql from ' + filename)
-    pg_env = os.environ.copy()
-    pg_env.update({'PGPASSWORD': dbconfig['password']})
-    cmd_str = 'psql -U {user} -d {database} -h {host} -f {filename}'.format(filename=filename, **dbconfig)
-    process = Popen(cmd_str, stdout=PIPE, stdin=PIPE, shell=True, env=pg_env)
-    stdout, stderr = process.communicate()
-    if stderr is None:
-        print(stdout.decode())
-    else:
-        print(stderr.decode())
-    return stderr is None
-
-
-def init_db():
-    print('Preparing database')
-    filename = os.path.join(PKG_ROOT, 'sql', 'script_prepare_db.sql')
-    run_sql_file(filename)
+ARGS = parser.parse_args()
 
 
 def import_data():
@@ -132,14 +99,14 @@ def import_data():
 def insert_data(filename, table, fields, get_values, get_items):
     print('Start processing ' + filename)
 
-    logname = os.path.join(PKG_ROOT, 'log', filename + '.log')
+    logname = os.path.join(ARGS.log_dir, filename + '.log')
     file = open(logname, 'w', encoding='utf-8')
 
     fields_str = ', '.join(str(v) for v in fields)
     dollars = ', '.join('$' + str(i + 1) for i in range(len(fields)))
     placeholders = ', '.join('%({0})s'.format(f) for f in fields)
 
-    tree = ElementTree.parse(os.path.join(XML_ROOT, filename))
+    tree = ElementTree.parse(os.path.join(ARGS.xml_dir, filename))
     items = get_items(tree)
     count = len(items)
     print('Found {0} items'.format(count))
@@ -147,7 +114,7 @@ def insert_data(filename, table, fields, get_values, get_items):
     sql_str = "EXECUTE prepared_query_{table} ({placeholders})".format(placeholders=placeholders, table=table)
     i = 0
 
-    with conn.cursor() as cur:
+    with CONN.cursor() as cur:
         sql = 'PREPARE prepared_query_{table} AS '.format(table=table) + \
               'INSERT INTO {tbl} ({fields}) VALUES ({dollars})' \
                   .format(fields=fields_str, dollars=dollars, tbl=table)
@@ -163,15 +130,35 @@ def insert_data(filename, table, fields, get_values, get_items):
             print('\rProgress: {0}% ({1})'.format(round(i / count * 100), i), end='', flush=True)
             file.write(str(values) + '\n')
         print()
-    conn.commit()
+    CONN.commit()
     file.close()
 
 
-def create_indexes():
-    print('Creating indexes')
-    filename = os.path.join(PKG_ROOT, 'sql', 'script_create_constraints.sql')
-    run_sql_file(filename)
+def check_xml_files():
+    source_filenames = [
+        'concepts.xml',
+        'relations.xml',
+        'text_entry.xml',
+        'synonyms.xml',
+    ]
+    for filename in source_filenames:
+        with open(os.path.join(ARGS.xml_dir, filename)) as f:
+            pass
 
 
-if __name__ == "__main__":
-    main(sys.argv[1:])
+try:
+    CONN = psycopg2.connect(ARGS.connection_string)
+except psycopg2.Error as err:
+    print("I'm not able to connect to the database")
+    print(err)
+    exit(1)
+
+try:
+    check_xml_files()
+except IOError as err:
+    print('Bad source files ({})'.format(err))
+    exit(1)
+
+import_data()
+
+print('Done')

28f56de578838e37f7ccefd0e71be3311e498388
German Lashevich <german.lashevich@tatar.ru>
2017-02-05 15:54:42 +0300

    Description for and fixes queries that find anomalies.

--- sql/possible_anomalies.sql
+++ sql/possible_anomalies.sql
@@ -1,26 +1,18 @@
+-- В версии xml-файлов РуТез, с которой я работал первый раз, были ошибочно заполнены некоторые свойства.
+-- В новых версиях это исправлено, но проверку всё же оставлю.
 SELECT *
 FROM text_entry
-WHERE synt_type IN ('10', '20');
---
-SELECT *
-FROM text_entry
-WHERE main_word IN ('10', '20');
---
+WHERE synt_type IN ('10', '20') OR main_word IN ('10', '20');
+
+-- Обнаружение повторяющихся текстовых входов
 SELECT
   name,
   count(id) cnt
 FROM text_entry
 GROUP BY name
-ORDER BY cnt DESC;
---
-SELECT *
-FROM text_entry
-WHERE id IN (752483, 752482, 750972);
---
-SELECT *
-FROM text_entry
-WHERE name = 'НЕНАСИЛЬСТВЕННЫЙ';
---
+HAVING count(id) > 1;
+
+-- Обнаружение незарегистрированных синтаксических типов
 SELECT *
 FROM text_entry
 WHERE synt_type NOT IN (
@@ -37,58 +29,48 @@ WHERE synt_type NOT IN (
   'Prdc',
   'N', 'NG', 'NGprep', 'Prep', 'PrepG', 'Pron', 'Prtc', 'V', 'VG', 'VGprep'
 );
---
+
+-- Обнаружение текстовых входов, не привязанных ни к одному понятию
 SELECT *
 FROM text_entry t
   LEFT JOIN synonyms s
     ON s.entry_id = t.id
-WHERE s.concept_id IS NULL;
---
+  LEFT JOIN concepts c
+    ON c.id = s.concept_id
+WHERE c.id IS NULL;
+
+-- Обнаружение понятий, не имеющих текстовых входов
+SELECT *
+FROM concepts c
+  LEFT JOIN synonyms s
+    ON s.concept_id = c.id
+  LEFT JOIN text_entry t
+    ON t.id = s.entry_id
+WHERE t.id IS NULL;
+
+-- Несоответствие количества элементов в поле lemma количеству элементов в поле pos_string
 SELECT *
 FROM text_entry
 WHERE
   array_length(regexp_split_to_array(pos_string, '\s+'), 1) <> array_length(regexp_split_to_array(lemma, '\s+'), 1);
---
+
+-- Несоответствие количества элементов в поле lemma количеству элементов в поле name
 SELECT *
 FROM text_entry
 WHERE
   array_length(regexp_split_to_array(name, '\s+-*\s*'), 1) <> array_length(regexp_split_to_array(lemma, '\s+'), 1);
---
+
+-- Не указано главное слово для текстовых входах, состоящих из нескольких слов
 SELECT *
 FROM text_entry
 WHERE array_length(regexp_split_to_array(lemma, '\s+'), 1) > 1
       AND main_word = '';
---
+
+-- Текстовые входы с одинаковым полем lemma
 SELECT
   lemma,
   array_agg(name),
   count(1) cnt
 FROM text_entry
 GROUP BY lemma
-ORDER BY cnt DESC;
---
-SELECT
-  t.id,
-  t.name
-FROM text_entry t
-WHERE NOT EXISTS(
-    SELECT 1
-    FROM synonyms s
-      INNER JOIN concepts c
-        ON c.id = s.concept_id
-    WHERE s.entry_id = t.id
-          AND c.id IS NOT NULL
-);
---
-SELECT
-  c.id,
-  c.name
-FROM concepts c
-WHERE NOT EXISTS(
-    SELECT 1
-    FROM synonyms s
-      INNER JOIN text_entry t
-        ON t.id = s.entry_id
-    WHERE s.concept_id = c.id
-          AND t.id IS NOT NULL
-);
+HAVING count(1) > 1;

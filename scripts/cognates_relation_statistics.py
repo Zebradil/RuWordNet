b3a380ec8f5081ca3887ac3db24a83eac2a3a18e
German Lashevich <german.lashevich@tatar.ru>
2017-02-19 19:10:54 +0300

    Refactoring cognates_relation script.

--- scripts/cognates_relation_statistics.py
+++ scripts/cognates_relation_statistics.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python3
+import argparse
 
+from lxml import etree
 from psycopg2 import connect, extras
 
-dbconfig = {
-    'database': 'ruthes',
-    'user': 'ruwordnet',
-    'password': 'ruwordnet',
-    'host': '127.0.0.1'
-}
+parser = argparse.ArgumentParser(description='Extract derivation relations from RuThes and RuWordNet.')
+parser.add_argument(
+    '-o',
+    '--output-file',
+    type=str,
+    help='In that file xml will be written',
+    default=None
+)
+connection_string = "host='localhost' dbname='ruwordnet' user='ruwordnet' password='ruwordnet'"
+parser.add_argument(
+    '-c',
+    '--connection-string',
+    type=str,
+    help="Postgresql database connection string ({})".format(connection_string),
+    default=connection_string
+)
+parser.add_argument(
+    '-t',
+    '--test',
+    help="Only show found relations, don't generate xml file",
+    action='store_true'
+)
 
-conn = connect(**dbconfig)
+ARGS = parser.parse_args()
+
+conn = connect(ARGS.connection_string)
 
 
 def prepare_search_cognates(cursor):
@@ -18,14 +38,16 @@ def prepare_search_cognates(cursor):
         FROM (
                SELECT
                  name,
-                 'synset' rel_name
+                 'synset' rel_name,
+                 'RuWordNet' source
                FROM senses
                WHERE id != $1
                      AND synset_id = $2
                UNION
                SELECT
                  se.name,
-                 sr.name
+                 sr.name,
+                 'RuWordNet' source
                FROM senses se
                  INNER JOIN synset_relations sr
                    ON sr.child_id = se.synset_id
@@ -33,7 +55,8 @@ def prepare_search_cognates(cursor):
                UNION
                SELECT
                  t2.name,
-                 r.name
+                 r.name,
+                 'RuThes' source
                FROM text_entry t1
                  INNER JOIN synonyms s1 ON s1.entry_id = t1.id
                  INNER JOIN relations r ON r.from_id = s1.concept_id
@@ -92,7 +115,29 @@ def prepare_search_cognates_transitionally(cursor):
     cursor.execute('PREPARE search_cognates_transitionally AS ' + sql)
 
 
+def prepare_search_sense(cursor):
+    sql = """
+        SELECT
+          id,
+          name,
+          synset_id
+        FROM senses
+        WHERE name = $1
+        ORDER BY meaning
+        LIMIT 1"""
+
+    cursor.execute('PREPARE search_sense AS ' + sql)
+
+
 def main():
+    test = ARGS.test
+    if not test:
+        # Testing if file is writeable
+        open(ARGS.output_file, 'w').close()
+        root = etree.Element('senses')
+    else:
+        root = None
+
     with conn.cursor(cursor_factory=extras.RealDictCursor) as cur, \
             conn.cursor(cursor_factory=extras.RealDictCursor) as cur2:
 
@@ -100,6 +145,10 @@ def main():
         prepare_search_cognates(cur2)
         print('prepare_search_cognates_transitionally', flush=True)
         prepare_search_cognates_transitionally(cur2)
+
+        if not test:
+            print('prepare_search_sense', flush=True)
+            prepare_search_sense(cur2)
 
         print('search collocations', flush=True)
         sql = """
@@ -116,6 +165,9 @@ def main():
             print(flush=True)
             print(row['name'], flush=True)
 
+            if not test:
+                lexemes = []
+
             params = {
                 'sense_id': row['id'],
                 'synset_id': row['synset_id'],
@@ -124,6 +176,8 @@ def main():
             cur2.execute('EXECUTE search_cognates(%(sense_id)s, %(synset_id)s, %(word)s)', params)
             for cognate in cur2.fetchall():
                 print(cognate['name'] + ': ' + cognate['rel_name'])
+                if not test:
+                    lexemes.append(cognate['name'])
 
             for name in ('ВЫШЕ', 'НИЖЕ', 'ЧАСТЬ', 'ЦЕЛОЕ'):
                 if name == 'ВЫШЕ':
@@ -147,6 +201,26 @@ def main():
                             ' → '.join(senses_chain['name_path']) + \
                             ' (' + senses_chain['parent_relation_name'] + ')'
                     print(chain)
+                    if not test:
+                        lexemes.append(senses_chain['name'])
+
+            if not test and lexemes:
+                x_sense = etree.SubElement(root, 'sense')
+                x_sense.set('name', row['name'])
+                x_sense.set('id', row['id'])
+                x_sense.set('synset_id', row['synset_id'])
+                x_rel = etree.SubElement(x_sense, 'derived_from')
+                for lexeme in set(lexemes):
+                    cur2.execute('EXECUTE search_sense(%(name)s)', {'name': lexeme})
+                    row_lexeme = cur2.fetchone()
+                    if row_lexeme:
+                        x_lexeme = etree.SubElement(x_rel, 'sense')
+                        x_lexeme.set('name', row_lexeme['name'])
+                        x_lexeme.set('id', row_lexeme['id'])
+                        x_lexeme.set('synset_id', row_lexeme['synset_id'])
+
+        tree = etree.ElementTree(root)
+        tree.write(ARGS.output_file, encoding="utf-8", pretty_print=True)
 
     print('Done')
 

8a7ac74f23b6f9de9f2701d23f6410272c1003f7
German Lashevich <german.lashevich@tatar.ru>
2017-02-22 22:27:41 +0300

    Add query for deleting hypernym/hyponym relation if exists class/instance relation.

--- sql/post-conversion.sql
+++ sql/post-conversion.sql
@@ -12,3 +12,15 @@ WHERE name = 'part meronym'
       AND exists(SELECT *
                  FROM synsets
                  WHERE id = sr.parent_id AND part_of_speech = 'V');
+
+-- Удаление отношений ВЫШЕ-НИЖЕ, если имеется отношение ЭКЗЕМПЛЯР-КЛАСС
+DELETE
+FROM synset_relations sr1
+WHERE name IN ('hypernym', 'hyponym')
+      AND exists(
+          SELECT *
+          FROM synset_relations sr2
+          WHERE sr2.parent_id = sr1.parent_id
+                AND sr2.child_id = sr1.child_id
+                AND sr2.name IN ('instance hypernym', 'instance hyponym')
+      );

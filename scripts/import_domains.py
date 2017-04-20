7f2c7f8bb96f8a782fc7f14359c2ef386ca5b1c5
German Lashevich <german.lashevich@gmail.com>
2017-04-20 22:51:01 +0300

    Fix domain relations lack issue

--- scripts/import_domains.py
+++ scripts/import_domains.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 from psycopg2 import connect
+import pprint
 
 revry_filename = 'in/revry_filtered.txt'
 rubrics_filename = 'in/rubrics_filtered.txt'
@@ -26,7 +27,9 @@ with open(revry_filename) as f:
         parts = line.strip().split(' ', 1)
         if len(parts) != 2:
             continue
-        revry.update(dict([reversed(parts)]))
+        if parts[1] not in revry:
+            revry[parts[1]] = []
+        revry[parts[1]].append(parts[0])
 
 conn = connect(**dbconfig)
 with conn.cursor() as cur:
@@ -36,13 +39,14 @@ with conn.cursor() as cur:
            VALUES ($1, $2, 'ДОМЕН')"""
     cur.execute(prepare)
 
-    for concept_id, domain_id in revry.items():
-        if domain_id in rubrics:
-            values = {
-                'from_id': concept_id,
-                'to_id': rubrics[domain_id],
-            }
-            cur.execute('EXECUTE insert_relation (%(from_id)s, %(to_id)s)', values)
-        else:
-            print('Not found: ', domain_id)
+    for concept_id, domains in revry.items():
+        for domain_id in domains:
+            if domain_id in rubrics:
+                values = {
+                    'from_id': concept_id,
+                    'to_id': rubrics[domain_id],
+                }
+                cur.execute('EXECUTE insert_relation (%(from_id)s, %(to_id)s)', values)
+            else:
+                print('Not found: ', domain_id)
     conn.commit()

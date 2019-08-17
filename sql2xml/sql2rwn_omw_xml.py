237ff9d8f418eb9f3cc1a72bfba8a3615eceb6e7
German Lashevich <german.lashevich@gmail.com>
2019-08-18 01:23:22 +0200

    First working version of OMW LMF generator

--- sql2xml/sql2rwn_omw_xml.py
+++ sql2xml/sql2rwn_omw_xml.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
 import argparse
-import os
+import logging
+from collections import defaultdict
 
 from lxml import etree
 from psycopg2 import connect, extras
 
-PKG_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 parser = argparse.ArgumentParser(description="Generate RuWordNet OMW xml files")
-connection_string = "host='localhost' dbname='ruwordnet' user='ruwordnet' password='ruwordnet'"
+connection_string = (
+    "host='localhost' dbname='ruwordnet' user='ruwordnet' password='ruwordnet'"
+)
 parser.add_argument(
     "-c",
     "--connection-string",
@@ -17,224 +18,294 @@ parser.add_argument(
     help="Postgresql database connection string ({})".format(connection_string),
     default=connection_string,
 )
-parser.add_argument(
-    "-o",
-    "--output-directory",
-    help="A directory where xml-files will be saved",
-    default=os.path.join(PKG_ROOT, "out", "rwn_omw"),
-)
+parser.add_argument("-l", "--level", help="Logging level", default=logging.INFO)
 
 ARGS = parser.parse_args()
 
+logging.basicConfig(level=ARGS.level)
+
 extras.register_uuid()
 
+allowed_synset_relations = {
+    "agent",
+    "also",
+    "antonym",
+    "attribute",
+    "be_in_state",
+    "causes",
+    "classified_by",
+    "classifies",
+    "co_agent_instrument",
+    "co_agent_patient",
+    "co_agent_result",
+    "co_instrument_agent",
+    "co_instrument_patient",
+    "co_instrument_result",
+    "co_patient_agent",
+    "co_patient_instrument",
+    "co_result_agent",
+    "co_result_instrument",
+    "co_role",
+    "direction",
+    "domain_region",
+    "domain_topic",
+    "entails",
+    "eq_synonym",
+    "exemplifies",
+    "has_domain_region",
+    "has_domain_topic",
+    "holo_location",
+    "holo_member",
+    "holo_part",
+    "holo_portion",
+    "holo_substance",
+    "holonym",
+    "hypernym",
+    "hyponym",
+    "in_manner",
+    "instance_hypernym",
+    "instance_hyponym",
+    "instrument",
+    "involved",
+    "involved_agent",
+    "involved_direction",
+    "involved_instrument",
+    "involved_location",
+    "involved_patient",
+    "involved_result",
+    "involved_source_direction",
+    "involved_target_direction",
+    "is_caused_by",
+    "is_entailed_by",
+    "is_exemplified_by",
+    "is_subevent_of",
+    "location",
+    "manner_of",
+    "mero_location",
+    "mero_member",
+    "mero_part",
+    "mero_portion",
+    "mero_substance",
+    "meronym",
+    "other",
+    "patient",
+    "restricted_by",
+    "restricts",
+    "result",
+    "role",
+    "similar",
+    "source_direction",
+    "state_of",
+    "subevent",
+    "target_direction",
+}
 
-class Generator:
-    def __init__(self, out_dir: str, connection):
-        self.connection = connection
-        self.out_dir = out_dir
-        self.synset_counter = 0
-        self.sense_counter = 0
-        self.synsets = []
-        self.senses = []
-        self.synset_relations = []
+allowed_sense_relations = {
+    "also",
+    "antonym",
+    "derivation",
+    "domain_region",
+    "domain_topic",
+    "exemplifies",
+    "has_domain_region",
+    "has_domain_topic",
+    "is_exemplified_by",
+    "other",
+    "participle",
+    "pertainym",
+    "similar",
+}
 
-    def proto_run(self):
-        tree = etree.Element("LexicalResource")
-        tree.attrib["xmlns:dc"] = "http://purl.org/dc/elements/1.1/"
-        lexicon = etree.Element(
-            "Lexicon",
-            id="RuWordNet",
-            label="RuWordNet",
-            language="ru",
-            email="john.doe@example.com",
-            license="proprietary",
-            version="1.0",
-            citation="TODO",
-            url="http://ruwordnet.ru",
+
+def sense_rel(relation):
+    return {"derived_from": "derivation"}.get(relation, relation)
+
+
+def synset_rel(relation):
+    return {
+        "cause": "causes",
+        "domain": "domain_topic",
+        "entailment": "entails",
+        "instance hypernym": "instance_hypernym",
+        "instance hyponym": "instance_hyponym",
+        "part holonym": "holo_part",
+        "part meronym": "mero_part",
+    }.get(relation, relation)
+
+
+def pos(part_of_speech):
+    return {"adj": "a"}.get(part_of_speech, part_of_speech)
+
+
+def synset_id(data):
+    return "-".join((str(data["concept_id"]), pos(data["part_of_speech"])))
+
+
+def sense_id(data):
+    return "-".join(
+        (
+            str(data["concept_id"]),
+            pos(data["part_of_speech"]),
+            str(data["text_entry_id"]),
         )
+    )
 
-    def run(self):
-        print("Start")
 
-        with self.connection.cursor(cursor_factory=extras.RealDictCursor) as cur:
-            print("Selecting all data...")
-            print("synsets")
+def run(connection):
+    nsmap = {"dc": "http://purl.org/dc/elements/1.1/"}
+    LexicalResource = etree.Element("LexicalResource", nsmap=nsmap)
+    Lexicon = etree.SubElement(
+        LexicalResource,
+        "Lexicon",
+        id="RuWordNet",
+        label="RuWordNet",
+        language="ru",
+        email="john.doe@example.com",
+        license="proprietary",
+        version="1.0",
+        citation="TODO",
+        url="http://ruwordnet.ru",
+    )
+
+    with connection.cursor(cursor_factory=extras.RealDictCursor) as cur:
+        logging.info("Selecting all data...")
+
+        logging.info("sense relations")
+        cur.execute(
+            """
+            SELECT
+                  ser.parent_id,
+                  ser.child_id,
+                  ser.name,
+                  c.id concept_id,
+                  LOWER(sy.part_of_speech) part_of_speech,
+                  t.id text_entry_id
+                FROM sense_relations ser
+                  JOIN senses se ON se.id = ser.child_id
+                  JOIN synsets sy ON sy.id = se.synset_id
+                  JOIN concepts c ON c.name = sy.name
+                  JOIN text_entry t ON t.name = se.name
+            """
+        )
+        sense_relations = defaultdict(list)
+        for row in cur:
+            sense_relations[row["parent_id"]].append(row)
+
+            logging.info("senses")
             cur.execute(
                 """
-              SELECT
-                synsets.*,
-                array_agg(senses.id) senses
-              FROM synsets
-              INNER JOIN senses
-                ON synsets.id = senses.synset_id
-              GROUP BY synsets.id
-              ORDER BY part_of_speech"""
+                SELECT
+                  se.id,
+                  se.synset_id,
+                  LOWER(se.name) "name",
+                  LOWER(se.lemma) lemma,
+                  t.id text_entry_id,
+                  LOWER(sy.part_of_speech) part_of_speech,
+                  c.id concept_id
+                FROM senses se
+                  JOIN synsets sy ON sy.id = se.synset_id
+                  JOIN concepts c ON c.name = sy.name
+                  JOIN text_entry t ON t.name = se.name
+                """
             )
-            rows = cur.fetchall()
-            self.synsets = [{**row, **{"relations": [], "index": self.gen_synset_index(row)}} for row in rows]
-            synsets_by_id = {synset["id"]: synset for synset in self.synsets}
+            lexical_entries = defaultdict(list)
+            for row in cur:
+                lexical_entries[row["lemma"]].append(row)
 
-            print("senses")
-            cur.execute("SELECT * FROM senses")
-            rows = cur.fetchall()
-            self.senses = {
-                row["id"]: {
-                    **row,
-                    **{
-                        "synset_id": synsets_by_id[row["synset_id"]]["index"],
-                        "id": self.gen_sense_index(row),
-                        "meaning": int(row["meaning"]) + 1,
-                    },
+            for lemma, senses in lexical_entries.items():
+                sense = senses[0]
+                LexicalEntry = etree.SubElement(
+                    Lexicon, "LexicalEntry", id=str(sense["text_entry_id"])
+                )
+                etree.SubElement(
+                    LexicalEntry,
+                    "Lemma",
+                    writtenForm=lemma,
+                    partOfSpeech=pos(sense["part_of_speech"]),
+                )
+                etree.SubElement(
+                    LexicalEntry, "Form", writtenForm=sense["name"]
+                )
+                for sense in senses:
+                    Sense = etree.SubElement(
+                        LexicalEntry,
+                        "Sense",
+                        id=sense_id(sense),
+                        synset=synset_id(sense),
+                    )
+
+                    for relation in sense_relations[sense["id"]]:
+                        args = {"target": sense_id(relation)}
+                        relType = sense_rel(relation["name"])
+                        if relType in allowed_sense_relations:
+                            args["relType"] = relType
+                        else:
+                            args["relType"] = "other"
+                            args["note"] = relType
+                        etree.SubElement(Sense, "SenseRelation", **args)
+
+            logging.info("synset relations")
+            cur.execute(
+                """
+                SELECT
+                  syr.parent_id,
+                  syr.child_id,
+                  syr.name,
+                  LOWER(sy.part_of_speech) part_of_speech,
+                  c.id concept_id
+                FROM synset_relations syr
+                  JOIN synsets sy ON sy.id = syr.child_id
+                  JOIN concepts c ON c.name = sy.name
+                """
+            )
+            synset_relations = defaultdict(list)
+            for row in cur:
+                synset_relations[row["parent_id"]].append(row)
+
+            logging.info("synsets")
+            cur.execute(
+                """
+                SELECT
+                  sy.id,
+                  sy.name,
+                  sy.definition,
+                  LOWER(sy.part_of_speech) part_of_speech,
+                  c.id concept_id,
+                  ili.wn_id,
+                  ili.wn_gloss
+                FROM synsets sy
+                 JOIN concepts c ON c.name = sy.name
+                 LEFT JOIN ili ON ili.concept_id = c.id
+                """
+            )
+            for synset in cur:
+                args = {
+                    "id": synset_id(synset),
+                    "partOfSpeech": pos(synset["part_of_speech"]),
                 }
-                for row in rows
-            }
+                if synset["wn_id"] is not None:
+                    args["ili"] = synset["wn_id"]
+                Synset = etree.SubElement(Lexicon, "Synset", **args)
 
-            print("synset relations")
-            cur.execute("SELECT * FROM synset_relations")
-            self.synset_relations = cur.fetchall()
+                for relation in synset_relations[synset["id"]]:
+                    args = {"target": synset_id(relation)}
+                    relType = synset_rel(relation["name"])
+                    if relType in allowed_synset_relations:
+                        args["relType"] = relType
+                    else:
+                        args["relType"] = "other"
+                        args["note"] = relType
+                    etree.SubElement(Synset, "SynsetRelation", **args)
 
-            print("distribute relations...")
-            for relation in self.synset_relations:
-                synset = synsets_by_id[relation["parent_id"]]
-                relation["parent_id"] = synset["index"]
-                relation["child_id"] = synsets_by_id[relation["child_id"]]["index"]
-                synset["relations"].append(relation)
-
-            current_pos = None
-            print("building trees...")
-            i = 0
-            count = len(self.synsets)
-            for synset in self.synsets:
-                if current_pos != synset["part_of_speech"]:
-                    if current_pos is not None:
-                        print()
-                        self.write_file(synsets_root, "synsets", current_pos)
-                        self.write_file(senses_root, "senses", current_pos)
-                        self.write_file(synset_relations_root, "synset_relations", current_pos)
-                    synsets_root = etree.Element("synsets")
-                    senses_root = etree.Element("senses")
-                    synset_relations_root = etree.Element("relations")
-                    current_pos = synset["part_of_speech"]
-                    print()
-                    print("POS: " + current_pos)
-                self.add_synset(synsets_root, synset)
-                for sense_id in synset["senses"]:
-                    self.add_sense(senses_root, self.get_sense(sense_id))
-                for relation in synset["relations"]:
-                    self.add_synset_relation(synset_relations_root, relation)
-                i += 1
-                print("\rProgress: {0}% ({1})".format(round(i / count * 100), i), end="", flush=True)
-            self.write_file(synsets_root, "synsets", current_pos)
-            self.write_file(senses_root, "senses", current_pos)
-            self.write_file(synset_relations_root, "synset_relations", current_pos)
-
-            print()
-            self.generate_composed_of_relations_file(cur)
-            self.generate_derived_from_relations_file(cur)
-
-        print("Done")
-
-    def write_file(self, root: etree.Element, entity: str, pos: str):
-        tree = etree.ElementTree(root)
-        filename = os.path.join(self.out_dir, "{0}.{1}.xml".format(entity, pos[0]))
-        if os.path.isfile(filename):
-            os.remove(filename)
-        print("Output file: " + filename)
-        tree.write(filename, encoding="utf-8", pretty_print=True)
-
-    def add_synset(self, root: etree.Element, row: dict):
-        synset = etree.SubElement(root, "synset")
-        synset.set("id", row["index"])
-        # synset.set('id', str(row['id']))
-        synset.set("ruthes_name", row["name"])
-        synset.set("definition", xstr(row["definition"]))
-        synset.set("part_of_speech", row["part_of_speech"])
-
-        for sense_id in row["senses"]:
-            sense = self.get_sense(sense_id)
-            sense_el = etree.SubElement(synset, "sense")
-            sense_el.set("id", str(sense["id"]))
-            sense_el.text = sense["lemma"]
-
-    def add_sense(self, root, row):
-        self.fill_element_attributes(etree.SubElement(root, "sense"), row)
-
-    def add_synset_relation(self, root, row):
-        self.fill_element_attributes(etree.SubElement(root, "relation"), row)
-
-    @staticmethod
-    def fill_element_attributes(element, attributes: dict):
-        for k, v in attributes.items():
-            element.set(k, xstr(v))
-
-    def get_sense(self, sense_id):
-        return self.senses[sense_id]
-
-    def get_relations(self, synset):
-        return [relation for relation in self.synset_relations if relation["parent_id"] == synset["id"]]
-
-    def gen_synset_index(self, row):
-        self.synset_counter += 1
-        return row["part_of_speech"][0] + str(self.synset_counter)
-
-    def gen_sense_index(self, row):
-        self.sense_counter += 1
-        return self.sense_counter
-
-    def generate_sense_relations_file(self, relation_name, cur):
-        print('Generating "{}" relations file'.format(relation_name))
-
-        print("Getting relations from the database")
-        sql = """
-          SELECT
-            parent_id,
-            array_agg(child_id) child_ids
-          FROM sense_relations
-          WHERE name = %s
-          GROUP BY parent_id"""
-        cur.execute(sql, (relation_name,))
-
-        root = etree.Element("senses")
-
-        print("Generating xml")
-        for row in cur:
-            parent_sense = self.senses[row["parent_id"]]
-            x_sense = etree.SubElement(root, "sense")
-            x_sense.set("name", xstr(parent_sense["name"]))
-            x_sense.set("id", xstr(parent_sense["id"]))
-            x_sense.set("synset_id", xstr(parent_sense["synset_id"]))
-            x_rel = etree.SubElement(x_sense, relation_name)
-            for child_id in row["child_ids"]:
-                child_sense = self.senses[child_id]
-                x_lexeme = etree.SubElement(x_rel, "sense")
-                x_lexeme.set("name", xstr(child_sense["name"]))
-                x_lexeme.set("id", xstr(child_sense["id"]))
-                x_lexeme.set("synset_id", xstr(child_sense["synset_id"]))
-
-        tree = etree.ElementTree(root)
-        filename = os.path.join(self.out_dir, "{}.xml".format(relation_name))
-        if os.path.isfile(filename):
-            os.remove(filename)
-        print("Output file: " + filename)
-        tree.write(
-            filename,
-            encoding="utf-8",
-            pretty_print=True,
-            xml_declaration=True,
-            doctype='<!DOCTYPE LexicalResource SYSTEM "http://globalwordnet.github.io/schemas/WN-LMF-1.0.dtd">',
+        print(
+            etree.tostring(
+                LexicalResource,
+                encoding="utf-8",
+                pretty_print=True,
+                xml_declaration=True,
+                doctype='<!DOCTYPE LexicalResource SYSTEM "http://globalwordnet.github.io/schemas/WN-LMF-1.0.dtd">',
+            ).decode()
         )
 
-    def generate_derived_from_relations_file(self, cur):
-        self.generate_sense_relations_file("derived_from", cur)
 
-    def generate_composed_of_relations_file(self, cur):
-        self.generate_sense_relations_file("composed_of", cur)
-
-
-def xstr(value):
-    return "" if value is None else str(value)
-
-
-generator = Generator(out_dir=ARGS.output_directory, connection=connect(ARGS.connection_string))
-generator.run()
+run(connect(ARGS.connection_string))

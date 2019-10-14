b1d73518530797dec4c3b0e659753dc635f9fd88
German Lashevich <german.lashevich@gmail.com>
2019-10-14 02:35:24 +0200

    Use one table for all versions of ILI mapping

--- scripts/import_ili_map_wn.py
+++ scripts/import_ili_map_wn.py
@@ -13,9 +13,8 @@ parser = argparse.ArgumentParser(
 connection_string = (
     "host='localhost' dbname='ruwordnet' user='ruwordnet' password='ruwordnet'"
 )
-parser.add_argument("-f", "--file", type=str, help="Source tab-separated file")
 parser.add_argument(
-    "-t", "--table", type=str, help="Database table name (will be truncated!)"
+    "-f", "--file", type=str, help="Source tab-separated file", required=True
 )
 parser.add_argument(
     "-c",
@@ -23,6 +22,13 @@ parser.add_argument(
     type=str,
     help="Postgresql database connection string ({})".format(connection_string),
     default=connection_string,
+)
+parser.add_argument(
+    "-v",
+    "--version",
+    help="Prinston Wordnet version (30, 31, ...)",
+    type=int,
+    required=True,
 )
 parser.add_argument(
     "-n", "--dry-run", help="Do not do changes in the database", action="store_true"
@@ -34,48 +40,11 @@ logging.basicConfig(level=logging.INFO)
 
 conn = connect(ARGS.connection_string)
 with open(ARGS.file) as file, conn.cursor() as cur:
-    cur.execute(
-        """
-        SELECT 1
-        FROM information_schema.tables
-        WHERE table_name=%s
-        LIMIT 1
-        """,
-        (ARGS.table,),
-    )
-    if cur.fetchone():
-        logging.info(
-            "Truncate table %s%s", ARGS.table, " (dry run)" if ARGS.dry_run else ""
-        )
-        if not ARGS.dry_run:
-            cur.execute(f"TRUNCATE {ARGS.table}")
-    else:
-        logging.info(
-            "Create table %s%s", ARGS.table, " (dry run)" if ARGS.dry_run else ""
-        )
-        if not ARGS.dry_run:
-            cur.execute(
-                f"""
-                CREATE TABLE {ARGS.table} (
-                    ili TEXT PRIMARY KEY,
-                    wn TEXT,
-                    extra TEXT
-                )
-                """
-            )
-
     inserted_lines = 0
     skipped_lines = 0
     for line in file:
-        if not line.startswith("<"):
-            continue
         parts = line.strip().split("\t")
-        match = re.search(r"^[^:]+:(\S+)\W+(.*)$", parts[2])
-        values = {
-            "ili": parts[0].strip("<>"),
-            "wn": match.group(1),
-            "extra": match.group(2),
-        }
+        values = {"ili": parts[0], "wn": parts[1], "version": ARGS.version}
 
         if ARGS.dry_run:
             logging.info(f"DRY-RUN: insert values {values}")
@@ -83,8 +52,8 @@ with open(ARGS.file) as file, conn.cursor() as cur:
             try:
                 cur.execute(
                     f"""
-                    INSERT INTO {ARGS.table} (ili, wn, extra)
-                    VALUES (%(ili)s, %(wn)s, %(extra)s)
+                    INSERT INTO ili_map_wn (ili, wn, version)
+                    VALUES (%(ili)s, %(wn)s, %(version)s)
                     """,
                     values,
                 )

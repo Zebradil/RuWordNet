7f5e88144d4cf5f52e04706851c71e27fc6af2a8
German Lashevich <german.lashevich@gmail.com>
2020-07-18 23:32:26 +0200

    Process verified ILI data

--- scripts/import_ili_data.py
+++ scripts/import_ili_data.py
@@ -20,32 +20,79 @@ parser.add_argument(
 parser.add_argument(
     "-n", "--dry-run", help="Do not do changes in the database", action="store_true"
 )
+parser.add_argument(
+    "--format",
+    default="auto",
+    const="auto",
+    nargs="?",
+    choices=("verified", "auto"),
+    help="Specify format of input data (default: %(default)s)",
+)
 
 ARGS = parser.parse_args()
 
 logging.basicConfig(level=logging.INFO)
 
+
+def parse_auto(line):
+    parts = line.split("\t")
+    return {
+        "link_type": parts[0],
+        "concept_id": parts[1],
+        "wn_lemma": parts[4],
+        "wn_id": parts[6],
+        "wn_gloss": parts[7],
+        "source": "auto",
+    }
+
+
+def parse_verified(line):
+    parts = line.split("\t")
+    concept_id = parts[0]
+    if concept_id[0] == "+":
+        concept_id = concept_id[1:]
+        return {
+            "link_type": None,
+            "concept_id": concept_id,
+            "wn_lemma": parts[3],
+            "wn_id": parts[5],
+            "wn_gloss": parts[6],
+            "source": "auto verified",
+        }
+    else:
+        return None
+
+
 conn = connect(ARGS.connection_string)
 with open(ARGS.file) as file, conn.cursor() as cur:
     inserted_lines = 0
     skipped_lines = 0
+    n = 0
     for line in file:
-        parts = line.strip().split("\t")
-        values = {
-            "link_type": parts[0],
-            "concept_id": parts[1],
-            "wn_lemma": parts[4],
-            "wn_id": parts[6],
-            "wn_gloss": parts[7],
-        }
+        n += 1
+
+        line = line.strip()
+        if line == "":
+            continue
+
+        if ARGS.format == "auto":
+            values = parse_auto(line.strip())
+        elif ARGS.format == "verified":
+            values = parse_verified(line.strip())
+        else:
+            raise ValueError(f"Invalid format option: {ARGS.format}")
+
+        if values is None:
+            logging.info("Skipping line %s", n)
+            continue
 
         if ARGS.dry_run:
             logging.info("DRY-RUN: insert values %s", values)
         else:
             try:
                 cur.execute(
-                    """INSERT INTO ili (link_type, concept_id, wn_lemma, wn_id, wn_gloss)
-                    VALUES (%(link_type)s, %(concept_id)s, %(wn_lemma)s, %(wn_id)s, %(wn_gloss)s)""",
+                    """INSERT INTO ili (link_type, concept_id, wn_lemma, wn_id, wn_gloss, source)
+                    VALUES (%(link_type)s, %(concept_id)s, %(wn_lemma)s, %(wn_id)s, %(wn_gloss)s, %(source)s)""",
                     values,
                 )
                 conn.commit()

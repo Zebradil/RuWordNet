cd7d6a065a8aca164c99730d63fe806a44a4dc7f
German Lashevich <german.lashevich@gmail.com>
2020-11-10 02:40:36 +0100

    Script for checking ILI relations for frequent words

---
+++ scripts/check_frequent_words_for_ili.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import logging
+import re
+import sys
+
+from psycopg2 import connect
+
+connection_string = (
+    "host='localhost' dbname='ruwordnet' user='ruwordnet' password='ruwordnet'"
+)
+
+logging.basicConfig(level=logging.INFO)
+
+conn = connect(connection_string)
+with conn.cursor() as cur:
+    re_sense_lemma = re.compile(r"^([A-ZА-Я\s-]+)")
+
+    for line in sys.stdin:
+        line = line.strip()
+        match = re_sense_lemma.match(line)
+        lemma = match.group(1).strip()
+
+        cur.execute("SELECT 1 FROM text_entry WHERE lemma = %s", (lemma,))
+        if cur.fetchone() is None:
+            print(f"#{line}")
+            continue
+
+        cur.execute(
+            """
+            SELECT 1
+            FROM text_entry t
+            JOIN synonyms s ON s.entry_id = t.id
+            JOIN ili ON ili.concept_id = s.concept_id
+            WHERE t.lemma=%s
+              AND ili.source IN('manual', 'auto verified')
+            LIMIT 1
+            """,
+            (lemma,),
+        )
+        if cur.fetchone():
+            print(f"+{line}")
+            continue
+
+        cur.execute(
+            """
+            SELECT 1
+            FROM text_entry t
+            JOIN synonyms s ON s.entry_id = t.id
+            JOIN ili ON ili.concept_id = s.concept_id
+            WHERE t.lemma=%s
+              AND ili.source NOT IN('manual', 'auto verified')
+            LIMIT 1
+            """,
+            (lemma,),
+        )
+        if cur.fetchone():
+            print(f"?{line}")
+            continue
+
+        print(f"?{line}")

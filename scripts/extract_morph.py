64eab0d90fa419918d88bf3f49b0d514b7886ea9
German Lashevich <german.lashevich@gmail.com>
2021-01-03 19:24:15 +0100

    Add script for extracting morph data

---
+++ scripts/extract_morph.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import argparse
+import csv
+import logging
+import sys
+
+import pymorphy2
+from psycopg2 import connect, extras
+
+parser = argparse.ArgumentParser(
+    description="Extracts morphological info from text_entries"
+)
+connection_string = (
+    "host='localhost' dbname='ruwordnet' user='ruwordnet' password='ruwordnet'"
+)
+parser.add_argument(
+    "-c",
+    "--connection-string",
+    type=str,
+    help="Postgresql database connection string ({})".format(connection_string),
+    default=connection_string,
+)
+
+ARGS = parser.parse_args()
+
+logging.basicConfig(level=logging.INFO)
+
+
+conn = connect(ARGS.connection_string)
+with conn.cursor(cursor_factory=extras.RealDictCursor) as cur:
+    cur.execute(
+        """
+            select c2.name concept_name, t.name entry_name, t.lemma
+            from (
+                select t2.*
+                from v2_text_entry t2
+                left join text_entry t on t.id = t2.id or t.name = t2.name or t.lemma = t2.lemma
+                where t.id is null
+            ) t
+            join v2_synonyms s on s.entry_id = t.id
+            join v2_concepts c2 on c2.id = s.concept_id
+            join concepts c on c.id = c2.id
+            where c2.id > 0 and not is_multiword(t.name)
+            order by 1, 2
+        """
+    )
+    morph = pymorphy2.MorphAnalyzer()
+    writer = csv.DictWriter(
+        sys.stdout,
+        fieldnames=["concept_name", "entry_name", "lemma", "POS"],
+        delimiter=",",
+        quotechar='"',
+    )
+    writer.writeheader()
+    for row in cur:
+        row["POS"] = morph.parse(row["lemma"])[0].tag.POS
+        writer.writerow(row)
